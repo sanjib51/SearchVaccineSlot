1cd544cdb9730dfc1d8446941d7e0d6f1e652376
sanjib51 <sanjib51@gmail.com>
2021-05-30 23:12:54 +0530

    publish to github page

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace AvailableVaccine.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 1 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 2 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 3 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 4 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 5 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 6 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 7 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 8 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 9 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using AvailableVaccine;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 10 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using AvailableVaccine.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using AvailableVaccine.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "D:\Workspace\AvailableVaccine\Pages\Counter.razor"
+#line 9 "D:\Workspace\SearchVaccineSlot\Pages\Counter.razor"
        
     private int currentCount = 0;
 
--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -12,77 +12,77 @@ namespace AvailableVaccine.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 1 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 2 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 3 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 4 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 5 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 6 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 7 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 8 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 9 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using AvailableVaccine;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Workspace\AvailableVaccine\_Imports.razor"
+#line 10 "D:\Workspace\SearchVaccineSlot\_Imports.razor"
 using AvailableVaccine.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Workspace\AvailableVaccine\Pages\FetchData.razor"
+#line 3 "D:\Workspace\SearchVaccineSlot\Pages\FetchData.razor"
 using System.Linq;
 
 #line default
@@ -97,7 +97,7 @@ using System.Linq;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 49 "D:\Workspace\AvailableVaccine\Pages\FetchData.razor"
+#line 49 "D:\Workspace\SearchVaccineSlot\Pages\FetchData.razor"
        
     private Rootobject forecasts;
     private DateTime localtime;
